06b0cd70794b3849fc8aa44db85486dbdd706d6d
Merlin Simoes <merlinsimoes@dhcp253.merunorth98.iit.edu>
2024-10-14 12:27:39 -0500

    Modified file

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate, Link } from 'react-router-dom'; // Import Link for navigation
 import './Dashboard.css'; // Make sure to have appropriate CSS styles
+import disasterImage from './assets/disasterimage.jpg';
 
 const Dashboard = () => {
   const navigate = useNavigate();
@@ -48,7 +49,7 @@ const Dashboard = () => {
 
       <div className="content-container" style={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: '20px', overflowY: 'auto' }}>
         <img
-          src="/Users/merlinsimoes/Documents/GraingerProject/disasterimage.jpg" // Replace with the actual image URL
+          src={disasterImage}// Replace with the actual image URL
           alt="Volunteers assisting in disaster management"
           style={{
             maxWidth: '100%', // Responsive image width
@@ -56,27 +57,6 @@ const Dashboard = () => {
             borderRadius: '5px', // Rounded corners for aesthetics
             boxShadow: '0 2px 5px rgba(0,0,0,0.1)', // Subtle shadow for depth
             marginBottom: '20px', // Space between images
-          }}
-        />
-        <img
-          src="https://example.com/image2.jpg" // Replace with the actual image URL
-          alt="Emergency supplies ready for distribution"
-          style={{
-            maxWidth: '100%', // Responsive image width
-            height: 'auto', // Maintain aspect ratio
-            borderRadius: '5px', // Rounded corners for aesthetics
-            boxShadow: '0 2px 5px rgba(0,0,0,0.1)', // Subtle shadow for depth
-            marginBottom: '20px', // Space between images
-          }}
-        />
-        <img
-          src="https://example.com/image3.jpg" // Replace with the actual image URL
-          alt="Community members participating in a disaster preparedness drill"
-          style={{
-            maxWidth: '100%', // Responsive image width
-            height: 'auto', // Maintain aspect ratio
-            borderRadius: '5px', // Rounded corners for aesthetics
-            boxShadow: '0 2px 5px rgba(0,0,0,0.1)', // Subtle shadow for depth
           }}
         />
       </div>
